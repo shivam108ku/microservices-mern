099c3f6a5b8d4680cde1b0a9958a8e1ba687b13e
Shivam Kumar <shivam108ku@gmail.com>
2025-11-03 17:02:52 +0530

    update description and service name in package.json and logger.ts

--- auth-service/src/config/logger.ts
+++ auth-service/src/config/logger.ts
@@ -4,7 +4,7 @@ import { Config } from '.';
 const logger = winston.createLogger({
     level: 'info',
     defaultMeta: {
-        serviceName: 'pizza',
+        serviceName: 'auth-service',
     },
     transports: [
         new winston.transports.File({
