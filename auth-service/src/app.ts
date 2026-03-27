ec667aad7fe92ace8970ae36daf9cd695f781906
Shivam <shivam108ku@gmail.com>
2026-03-27 19:32:41 +0530

    refactor: remove commented code and clean up app.ts

--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -1,21 +1,17 @@
 import express, { Request, Response } from 'express';
 import logger from './config/logger';
 import authRouter from './routes/auth';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import createHttpError, { HttpError } from 'http-errors';
 import { NextFunction } from 'connect';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    // const err = createHttpError(401, 'Cant access');
-    // next(err);
     res.send('Welcome Shivam');
 });
 
-app.use('/auth', authRouter);
+app.use('/auth', authRouter); 
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
     logger.error(err.message);
     const statusCode = err.statusCode || 500;
